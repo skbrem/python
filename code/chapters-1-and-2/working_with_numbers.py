7a9034fe3accb2f158e99291ddd0144492d9482e
Sean Bremner <sean@bremner.me>
2026-02-11 08:12:12 +0200

    made some changes

--- code/chapters-1-and-2/working_with_numbers.py
+++ code/chapters-1-and-2/working_with_numbers.py
@@ -1,6 +1,13 @@
+numbers = 0
+
 while True:
     num = int(input("Please type in integer number. Type in 0 to finish: "))
     if num == 0:
         break
     print(f"Number: {num}")
+    numbers = num + 1
+
+print(f"The program asks for numbers...")
 print(f"Number: {num}")
+print(f"Numbers typed in: {numbers}")
+
